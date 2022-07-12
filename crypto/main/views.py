4ed4bf5df8d358d2d4319e8e3ca9977e09ba93a3
Mindaugas <mindaugas@localhost.local>
2022-07-12 22:21:16 +0300

    minor

--- crypto/main/views.py
+++ crypto/main/views.py
@@ -21,3 +21,33 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'register.html', context)
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+#LE
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class LoginPageView(TemplateView):
+    template_name = "login.html"
+
+class LogedinPageView(TemplateView):
+    template_name = "logedin.html"
+
+
+'''
+from django.http import HttpResponse
+
+
+def homePageView(request):
+    return HttpResponse("Hello, World!")
+
+'''
