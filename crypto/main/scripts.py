069a8216c05f45cad1352109a967ac332480ee6f
Rugile Koveraite <rugileko@gmail.com>
2022-07-14 21:46:57 +0300

    custom commands

--- crypto/main/scripts.py
+++ crypto/main/scripts.py
@@ -2,34 +2,35 @@ import requests
 import os
 import pandas as pd
 import csv
-from crypto.main.models import Detail
+from .models import Detail
 
-api_key = os.getenv('CNODG5LTW3G2W1Z4')
-base_url = 'https://www.alphavantage.co/query?'
-function = 'DIGITAL_CURRENCY_DAILY'
-symbol = 'BTC'
-outputsize = 'full'
-market = 'USD'
-datatype = 'csv'
-response = requests.get(
-    f'{base_url}function={function}&symbol={symbol}&market={market}&apikey={api_key}&datatype={datatype}')
-# print(response.content)
-# print(response.json())
 
-csv_string = response.content.decode('utf-8')
-# print(csv_string)
-lines = csv_string.splitlines()
-reader = csv.reader(lines)
-parsed_csv = list(reader)
-reader = csv.reader(lines)
-parsed_data = list(reader)
+def get_data():
+    api_key = os.getenv('CNODG5LTW3G2W1Z4')
+    base_url = 'https://www.alphavantage.co/query?'
+    function = 'DIGITAL_CURRENCY_DAILY'
+    symbol = 'BTC'
+    outputsize = 'full'
+    market = 'USD'
+    datatype = 'csv'
+    response = requests.get(
+        f'{base_url}function={function}&symbol={symbol}&market={market}&apikey={api_key}&datatype={datatype}')
+    # print(response.content)
+    # print(response.json())
 
-list_of_ds_objects = []
-for row in parsed_data[1:]:
-    crypto_row = Detail(time_stamp=row[0] , open=row[1], high=row[2], low=row[3], close=row[4], volume=row[5], market_up=row[6], created_cap=row[7], currency_name=1)
-    crypto_row.save()
-    
+    csv_string = response.content.decode('utf-8')
+    # print(csv_string)
+    lines = csv_string.splitlines()
+    reader = csv.reader(lines)
+    parsed_csv = list(reader)
+    reader = csv.reader(lines)
+    parsed_data = list(reader)
 
+    list_of_ds_objects = []
+    for row in parsed_data[1:]:
+        crypto_row = Detail(time_stamp=row[0], open=row[1], high=row[2], low=row[3], close=row[4], volume=row[5],
+                            market_up=row[6], created_cap=row[7], currency_name=1)
+        crypto_row.save()
 
 # print(parsed_csv)
 
