36507e5b6189e487b8e5f286ad8d684386799fbf
Mindaugas <mindaugas@localhost.local>
2022-07-19 19:59:02 +0300

    updated script

--- crypto/main/management/commands/populatedb.py
+++ crypto/main/management/commands/populatedb.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand
-from main.scripts import get_data
-
+from main.scripts import run_script
+import subprocess
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('symbol', type=str)
+        parser.add_argument('market', type=str)
+
     def handle(self, **options):
-        get_data()
+       run_script(options["symbol"], options["market"]) 
--- crypto/main/scripts.py
+++ crypto/main/scripts.py
@@ -2,41 +2,32 @@ import requests
 import os
 import pandas as pd
 import csv
+import argparse
 from .models import Crypto, Detail
 
+base_url = 'https://www.alphavantage.co/query?'
+function = 'DIGITAL_CURRENCY_DAILY'
 
-def get_data():
-    api_key = os.getenv('CNODG5LTW3G2W1Z4')
-    base_url = 'https://www.alphavantage.co/query?'
-    function = 'DIGITAL_CURRENCY_DAILY'
-    symbol = 'BTC'
-    outputsize = 'full'
-    market = 'USD'
-    datatype = 'csv'
-    response = requests.get(
-        f'{base_url}function={function}&symbol={symbol}&market={market}&apikey={api_key}&datatype={datatype}')
-    # print(response.content)
-    # print(response.json())
+def get_raw_response(symbol, market, base_url=base_url, function=function):
+    return requests.get(
+        f'{base_url}function={function}&symbol={symbol}&market={market}&apikey={None}&datatype=csv')
 
+def parse_response(response):
     csv_string = response.content.decode('utf-8')
-    # print(csv_string)
     lines = csv_string.splitlines()
     reader = csv.reader(lines)
-    parsed_csv = list(reader)
-    reader = csv.reader(lines)
-    parsed_data = list(reader)
+    return list(reader)
+    
+# def upload_to_database(data):
+#     list_of_ds_objects = []
+#     for row in data[1:]:
+#         crypto_row = Detail(time_stamp=row[0], open=row[1], high=row[2], low=row[3], close=row[4], volume=row[5],
+#                             market_cap=row[6], created_at=row[7])
+#         crypto_row.save()
 
-    list_of_ds_objects = []
-    for row in parsed_data[1:]:
-        crypto_row = Detail(time_stamp=row[0], open=row[1], high=row[2], low=row[3], close=row[4], volume=row[5],
-                            market_cap=row[6], created_at=row[7])
-        crypto_row.save()
+def run_script(symbol, market):
+    response = get_raw_response(symbol, market)
+    data = parse_response(response)
+    print("----")
+    print(data)
 
-# print(parsed_csv)
-
-# with open('BTC.csv', 'wb') as file:
-#     file.write(response.content)
-# df = pd.read_csv('BTC.csv')  # Create pandas dataframe
-#
-# df.set_index('timestamp', inplace=True)  # Time-series index
-# print(df)
