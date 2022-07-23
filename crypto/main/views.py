6ea08d06ebbb394f77123eb4c62dca4b7e9c255e
Rugile Koveraite <rugileko@gmail.com>
2022-07-23 12:59:55 +0300

    views.py and graph.html modified

--- crypto/main/views.py
+++ crypto/main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .forms import UserRegistrationForm
-from .strategy import get_moving_avarage, get_date_range
+from .strategy import get_moving_average, get_date_range
+import seaborn as sns
 
 
 def home(request):
@@ -26,10 +27,16 @@ def register(request):
 
 def graph_view(request, vardas):
     date_range = get_date_range(numdays=10)
-    list_of_moving_avarage = []
+    list_of_moving_average = []
     for day in date_range:
-        ma = get_moving_avarage(date=day, numdays=50, crypto="BTC", currency="EUR")
-        list_of_moving_avarage.append(ma) 
+        ma = get_moving_average(date=day, numdays=50, crypto="BTC", currency="EUR")
+        list_of_moving_average.append(ma)
 
-    context = {'labas': 'labas vakaras', 'vardas': vardas, 'average': list_of_moving_avarage}
+    moving_graph = sns.lineplot(data=list_of_moving_average, x='date',
+                                y='ma', legend='full')
+    fig = moving_graph.figure
+    graph_file = BytesIO()
+    fig.savefig(graph_file, format='png')
+    encoded_file = base64.b64encode(graph_file.getvalue())
+    context = {'labas': 'labas vakaras', 'vardas': vardas, 'average': list_of_moving_average, 'png_file': encoded_file}
     return render(request, 'graph.html', context)
